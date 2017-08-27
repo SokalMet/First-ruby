65a7774586994abd92757e70dbc3b9b88e67a9d0
SokalMet <sokalmet@ukr.net>
2017-08-28 00:13:57 +0300

    moved back and rescaffolded

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170810201012) do
+ActiveRecord::Schema.define(version: 20170827210650) do
 
   create_table "products", force: :cascade do |t|
     t.string "title"
     t.text "description"
     t.string "image_url"
-    t.decimal "price", precision: 8, scale: 2
+    t.decimal "price"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
