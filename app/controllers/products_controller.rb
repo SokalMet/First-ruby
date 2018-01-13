6c7bbac87b528340d8c99caabe6e566c95104087
SokalMet <sokalmet@ukr.net>
2018-01-13 15:53:03 +0200

    update seeds.rb, adjust to show 6 products

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -6,7 +6,7 @@ class ProductsController < ApplicationController
   # GET /products
   # GET /products.json
   def index
-    @products = Product.order('created_at DESC').page(params[:page]).per_page(10)
+    @products = Product.order('created_at DESC').page(params[:page]).per_page(6)
   end
 
   # GET /products/1
