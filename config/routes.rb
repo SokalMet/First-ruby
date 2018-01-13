9630ec3f89bf7daf01e94ba89f8ed02645bde409
SokalMet <sokalmet@ukr.net>
2018-01-13 13:55:08 +0200

    routing fix: show action added

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
   resources :carts
   get 'store/index'
 
-  resources :products, only: [:index, :new, :create, :edit, :update, :destroy]
+  resources :products, only: [:index, :show, :new, :create, :edit, :update, :destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'store#index', as: 'store'
 end
