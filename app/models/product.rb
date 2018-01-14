96b1b3ffbc99220850b898c67dd5a859be8154d9
SokalMet <sokalmet@ukr.net>
2018-01-15 01:14:09 +0200

    product new page started.

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,15 +1,23 @@
 class Product < ActiveRecord::Base
   has_many :line_items
+  after_initialize :init
 
   before_destroy :ensure_not_referenced_by_any_line_item
-  validates :title, :length => { :minimum => 10, message: ': add 10 letters'}
-  validates :title, :description, :image_url, presence: true
+  validates :title, :length => { :minimum => 5, message: ': add 5 letters'}
+  validates :title, :description, presence: true
   validates :price, numericality: {greater_than_or_equal_to: 0.01}
   validates :title, uniqueness: true
   validates :image_url, allow_blank: true, format: {
       with: %r{\.(gif|jpg|png)\Z}i,
       message: 'URL should include formats: jpg, gif, png'
   }
+
+  def init
+    if self.image_url==""
+      self.image_url = 'ImageNotFound.png'
+    end
+  end
+
   def ensure_not_referenced_by_any_line_item
     if line_items.empty?
       return true
