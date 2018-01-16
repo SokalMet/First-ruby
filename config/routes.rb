b4e4c34eea5350b8756adb4765c3b763d1cba390
SokalMet <sokalmet@ukr.net>
2018-01-16 16:27:49 +0200

    add confirmable to devise, css update for error explanation

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
   resources :line_items
   resources :carts
   get 'store/index'
-
+  get '/users' => redirect('/users/sign_up')
   resources :products, only: [:index, :show, :new, :create, :edit, :update, :destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'store#index', as: 'store'
